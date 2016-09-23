9b875fa8b34d9988eb91b24c4fe4bfe50e728cd1
karimero-ma <kokubomasaki@masaki-mba.local>
2016-09-23 10:22:06 +0900

    行数出力に対応

--- src/main/java/jp/co/km/finder/Finder.java
+++ src/main/java/jp/co/km/finder/Finder.java
@@ -1,6 +1,7 @@
 package jp.co.km.finder;
 
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,21 +13,20 @@ import jp.co.km.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Finder {
 
 	private static Logger log = LoggerFactory.getLogger(Finder.class);
-	
+
 	public static void main(String[] args) {
 		new Finder().find(args);
 	}
-	
-	private void find(String[] args){
+
+	private void find(String[] args) {
 		FindCommand fc = new FindCommand();
 		fc.setPath(args[0]);
 		fc.setKeyword("margin");
 		fc.setFileNameRule("*.css");
-		
+
 		List<Path> paths = null;
 		try {
 			paths = FileUtils.createFileList(fc);
@@ -35,19 +35,19 @@ public class Finder {
 			System.exit(-1);
 		}
 		log.info("ファイル候補の取得終了");
-		
-		if(paths.isEmpty()){
+
+		if (paths.isEmpty()) {
 			log.info("対象ファイルがありません");
 			System.exit(0);
 		}
-		
-		if(log.isDebugEnabled()){
-			paths.stream().forEach(p->log.debug(p.toString()));
+
+		if (log.isDebugEnabled()) {
+			paths.stream().forEach(p -> log.debug(p.toString()));
 		}
-		
+
 		log.info("一致する文字列の検索");
 		List<Result> results = phaseFindText(fc, paths);
-		
+
 		log.info("----------- 結果の出力 -----------");
 		results.stream()
 			.filter(r -> !r.getLines().isEmpty())
@@ -58,29 +58,30 @@ public class Finder {
 		r.getLines().stream()
 			.forEach(l -> log.info("{} : {}", l.getNo(), l.getText()));
 	}
+
 	private List<Result> phaseFindText(FindCommand fc, List<Path> paths) {
 		return paths.stream()
-			.map(path -> {return parse(fc, path);})
+			.map(path -> parse(fc, path))
 			.collect(Collectors.toList());
 	}
-	
+
 	private Result parse(FindCommand fc, Path path) {
 		Result r = new Result();
 		r.setPath(path);
-		
-		List<Result.Line> lines;
-		try {
-			//TODO: ファイルのクローズ処理
-			log.debug("ファイルの読み込み開始 {}", path);
-			lines = Files.lines(path, Charset.forName("UTF8"))
+
+		log.debug("ファイルの読み込み開始 {}", path);
+		try (LineNumberReader reader = new LineNumberReader(
+				Files.newBufferedReader(path, Charset.forName("UTF-8")))) {
+			List<Result.Line> lines = reader.lines()
 				.filter(line -> fc.match(line))
-				.map(line -> Result.newLine(0, line))
+				.map(line -> Result.newLine(reader.getLineNumber(), line))
 				.collect(Collectors.toList());
-			
 			r.setLines(lines);
 		} catch (IOException e) {
-			log.error(path.toFile().getAbsolutePath() + "の解析に失敗しました。", e);
+			log.error("ファイルの解析に失敗しました １{}", path, e);
+			e.printStackTrace();
 		}
+
 		return r;
 	}
 }
