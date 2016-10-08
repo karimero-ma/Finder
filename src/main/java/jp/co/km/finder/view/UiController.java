477d1e5c4b548d0bddad6cdc22560c7fea00b2f6
karimero-ma <kokubomasaki@masaki-mba.local>
2016-10-08 17:52:41 +0900

    [update] #3 検索結果をテーブルに格納

--- src/main/java/jp/co/km/finder/view/UiController.java
+++ src/main/java/jp/co/km/finder/view/UiController.java
@@ -1,6 +1,7 @@
 package jp.co.km.finder.view;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -13,20 +14,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
 import jp.co.km.finder.FindCommand;
+import jp.co.km.finder.FindCommand.SEARCH_OPTION;
 import jp.co.km.finder.Finder;
 import jp.co.km.finder.InvalidFileCommandException;
 import jp.co.km.finder.Result;
-import jp.co.km.finder.FindCommand.SEARCH_OPTION;
+import jp.co.km.finder.view.ResultModel.ResultModelBuilder;
 
 public class UiController {
 	
-	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
-
 	private static final Logger log = LoggerFactory.getLogger(UiController.class);
 	
 	private static final ObservableList<String> EXTENTIONS 
@@ -34,47 +37,113 @@ public class UiController {
 		"*.*", "*.txt", "*.html", "*.css", "*.scss"
 		, "*.csv", "*.js", "*.java", "*.jsp", "*.xml");
 
+	/*---------------------------------------------------*/
+	/* Path                                              */
+	/*---------------------------------------------------*/
 	@FXML
 	TextField pathTxt;
 	
 	@FXML
 	Button choosePathBtn;
 	
+	/*---------------------------------------------------*/
+	/* SearchOptions                                     */
+	/*---------------------------------------------------*/
 	@FXML
+	/** サブフォルダーも検索 */
 	ToggleButton recursiveToggleButton;
 	
 	@FXML
+	/** 大文字・小文字を区別しない */
 	ToggleButton caseInsensitiveToggleButton;
 	
 	@FXML
+	/** 正規表現を使用する */
 	ToggleButton regexToggleButton;
 	
 	@FXML
+	/** 単語単位で検索する */
 	ToggleButton wordUnitToggleButton;
-	
-	@FXML
-	TextField patternTxt;
-	
-	@FXML
-	Button findBtn;
 	
 	@FXML
 	ComboBox<String> nameFilterComboBox;
 	
+	/*---------------------------------------------------*/
+	/* Execute Search                                    */
+	/*---------------------------------------------------*/
 	@FXML
-	TextArea resultTextArea;
+	/** 検索する文字列 */
+	TextField patternTxt;
+	
+	@FXML
+	/** 検索ボタン */
+	Button findBtn;
+	
+	/*---------------------------------------------------*/
+	/* StatusMessage                                     */
+	/*---------------------------------------------------*/
+	@FXML
+	/** お知らせメッセージ */
+	Label message;
+	
+	/*---------------------------------------------------*/
+	/* ResultView                                        */
+	/*---------------------------------------------------*/
+	@FXML
+	/** 検索結果を表示するテーブル */
+	TableView<ResultModel> resultView;
 
 	@FXML
+	/**
+	 * メインフレームの初期化
+	 */
 	void initialize(){
 		nameFilterComboBox.setItems(EXTENTIONS);
+		ObservableList<TableColumn<ResultModel, ?>> columns = resultView.getColumns();
+		
+		columns.get(0).setCellValueFactory(new PropertyValueFactory<>("path"));
+		columns.get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+		columns.get(2).setCellValueFactory(new PropertyValueFactory<>("no"));
+		columns.get(3).setCellValueFactory(new PropertyValueFactory<>("text"));
 	}
 	
 	@FXML
+	/**
+	 * 検索ボタンクリック時のイベント
+	 * @param event
+	 */
 	public void onclickFindBtn(ActionEvent event){
 		log.info("call onclickFindBtn");
 		
-		resultTextArea.clear();
+		FindCommand fc = createFileCommand();
+		try{
+			fc.validateRequired();
+		}catch (InvalidFileCommandException e) {
+			println("検索文字列を指定してください");
+			return;
+		}
 		
+		println("検索　開始 …");
+		List<Result> results = new Finder().find(fc);
+		println("検索　終了");
+		
+		log.trace("検索結果の描画開始");
+		final List<ResultModel> srcModels = new ArrayList<>();
+		results.stream().forEach(r -> {
+			ResultModelBuilder builder = new ResultModelBuilder();
+			builder.setPath(r.getPath().toString())
+				.setName(r.getPath().getFileName().toString());
+				
+			r.getLines().stream().forEach(l ->{
+				builder.setNo(Long.toString(l.getNo())).setText(l.getText().trim());
+				srcModels.add(builder.build());
+			});
+		});
+		resultView.setItems(FXCollections.observableArrayList(srcModels));
+		log.trace("検索結果の描画開始");
+	}
+
+	private FindCommand createFileCommand() {
 		FindCommand fc = new FindCommand();
 		fc.setPath(pathTxt.getText())
 			.setRecursive(recursiveToggleButton.isSelected())
@@ -94,30 +163,14 @@ public class UiController {
 		}
 				
 		log.info(fc.toString());
-		
-		try{
-			fc.validateRequired();
-		}catch (InvalidFileCommandException e) {
-			println("検索文字列を指定してください");
-			return;
-		}
-		
-		println("検索　開始 …");
-		
-		List<Result> results = new Finder().find(fc);
-		
-		results.forEach(r -> {
-			StringBuilder resultsBuf = new StringBuilder();
-			resultsBuf.append(r.getPath());
-			resultsBuf.append(LINE_SEPARATOR);
-			r.getLines().stream()
-				.forEach(l -> resultsBuf.append(String.format("%s : %s%s", l.getNo(), l.getText().trim(), LINE_SEPARATOR)));
-			resultTextArea.appendText(resultsBuf.toString());
-		});
-		println("検索　終了");
+		return fc;
 	}
 	
 	@FXML
+	/**
+	 * (パスの)選択ボタンクリック時のイベント
+	 * @param event
+	 */
 	public void onclickChoosePathBtn(ActionEvent event){
 		log.info("call onclickChoosePathBtn");
 		DirectoryChooser chooser = new DirectoryChooser();
@@ -131,9 +184,12 @@ public class UiController {
 		}
 	}
 	
+	/**
+	 * メッセージバーに通知を表示する
+	 * @param text
+	 */
 	private void println(String text){
-		resultTextArea.appendText(text);
-		resultTextArea.appendText(LINE_SEPARATOR);
+		message.setText(text);
 	}
 }
 
